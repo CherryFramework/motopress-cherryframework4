046acaa5669872249dce0a24cf7283d623e2544d
cheh <chehovskiy.dima@gmail.com>
2015-12-10 12:53:10 +0200

    UPD: version bump to 1.1.6.1-beta

--- motopress-cherryframework4.php
+++ motopress-cherryframework4.php
@@ -3,7 +3,7 @@
  * Plugin Name: MotoPress and CherryFramework 4 Integration
  * Plugin URI: http://www.getmotopress.com/
  * Description: Extend MotoPress Content Editor plugin with CherryFramework 4 shortcodes.
- * Version: 1.1.6
+ * Version: 1.1.6.1-beta
  * Author: MotoPress & Cherry Team
  * Author URI: http://www.getmotopress.com/
  * License: GPL2 or later
@@ -56,7 +56,7 @@ class MPCE_Cherry4 {
 	}
 
 	public function constants() {
-		define( 'MOTO_CHERRY4_VERSION', '1.1.6' );
+		define( 'MOTO_CHERRY4_VERSION', '1.1.6.1-beta' );
 		define( 'MOTO_CHERRY4_SLUG', basename( dirname( __FILE__ ) ) );
 	}
 
